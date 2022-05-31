66dfa17dd2a113cf4e3bc01a748e8d4115323e7f
rahulmistry751 <rahulmistry751@gmail.com>
2022-05-31 21:45:24 +0530

    feat:add authentication route

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ProductProvider } from "./context/product-context";
+import { AuthContextProvider } from "./context/auth-context";
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+    <AuthContextProvider>
       <ProductProvider>
         <App />
       </ProductProvider>
+    </AuthContextProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,62 +1,87 @@
 import "../auth.css";
-import React from "react";
-import { Link } from "react-router-dom";
+import React,{useEffect,useState} from "react";
+import { Link, useNavigate,useLocation } from "react-router-dom";
+import { useAuth } from "../../../context/auth-context";
+const testCredential={
+  "email":"testcred@gmail.com",
+  "password":"Test1234"
+}
 const Login = () => {
+  const location=useLocation();
+  const navigate=useNavigate();
+  const [loginCredential,setLoginCredential]=useState({email:"",password:""})
+  const {loginHandler,authError,userToken}=useAuth();
+  const loginTestCredentails=()=>{
+    setLoginCredential({email:testCredential.email,password:testCredential.password})
+  }
+  const changeHandler=(e)=>{
+  const {name,value}=e.target;
+    setLoginCredential({...loginCredential,[name]:value})
+  }
+const submitHandler=(e)=>{
+  e.preventDefault();
+  loginHandler(loginCredential);
+}
+  useEffect(()=>{
+    if(userToken){
+      navigate(location.state?.from?.pathname || '/',{replace:true})
+    }
+
+  },[userToken])
   return (
-    <div class="auth login">
-      <div class="card text-only card-login">
-        <div class="card-header h3 fw-600 mg-16 txt-c">Log in to BookAlley</div>
-        <div class="card-content txt-l">
-          <form action="">
-            <div class="login-username">
-              <label for="username" class="control-label gray fs-12">
-                Username
+    <div className="auth login">
+      <div className="card text-only card-login">
+        <div className="card-header h3 fw-600 mg-16 txt-c">Log in to BookAlley</div>
+        <div className="card-content txt-l">
+          <form action="" onSubmit={submitHandler}>
+            <div className="login-username">
+              <label htmlFor="email" className="control-label gray fs-12">
+                Email
               </label>
               <input
+                value={loginCredential.email}
                 type="text"
-                class="input form-control"
-                id="username"
-                name="username"
+                className="input form-control"
+                id="email"
+                name="email"
+                required
+                onChange={(e)=>changeHandler(e)}
               />
             </div>
-            <div class="login-password">
-              <label for="password" class="control-label gray fs-12">
+            <div className="login-password">
+              <label htmlFor="password" className="control-label gray fs-12">
                 Password
               </label>
               <input
+                value={loginCredential.password}
                 type="password"
-                class="input form-control"
+                className="input form-control"
                 id="password"
                 name="password"
-                autocomplete="off"
+                autoComplete="off"
+                required
+                onChange={(e)=>changeHandler(e)}
               />
             </div>
-            <div class="secondary-action txt-r fs-12">
-              <span class="save-login">
-                <input
-                  type="checkbox"
-                  class="input-checkbox checkbox-remember"
-                  id="remember-me"
-                />
-                <label for="remember-me" class="gray">
-                  Remember me
-                </label>
-              </span>
-              <a href="#" class="forgot-pass">
-                Forgot Password
-              </a>
+            {authError && <div className="error">{authError}</div>}
+            {
+              <div className="primary-action txt-c">
+              <button className="button button-full mgb-16" type="submit" onClick={loginTestCredentails}>
+                Use test credential
+              </button>
             </div>
-            <div class="primary-action txt-c">
-              <button class="button button-full" type="submit">
+            }
+            <div className="primary-action txt-c">
+              <button className="button button-full" type="submit">
                 Log In
               </button>
             </div>
           </form>
         </div>
       </div>
-      <div class="create-account">
+      <div className="create-account">
         <span>New to BookAlley? </span>
-        <Link to="/signup" class="fw-600">
+        <Link to="/signup" className="fw-600">
           Sign Up
         </Link>
       </div>
